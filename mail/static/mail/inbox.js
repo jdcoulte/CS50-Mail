428ee3b1356f629d625954763c53f7eedcb63584
Jeff Coulter <jeff.coulter1@gmail.com>
2020-08-24 16:18:05 -0700

    adding javascript

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -4,7 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose').addEventListener('click', () => compose_email);
+
+  // Event listener for send email button
+  document.querySelector('#compose-form').addEventListener('click', send_email);
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -31,3 +34,35 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+
+// Not sure if this is working yet. Need to come back to it.
+function send_email() {
+  console.log("Sent!");
+  let recipientsvalue = document.querySelector('#compose-recipients').value;
+  let subjectvalue = document.querySelector('#compose-subject').value;
+  let bodyvalue = document.querySelector('#compose-body').value;
+
+  console.log(recipientsvalue);
+  console.log(subjectvalue);
+  console.log(bodyvalue);
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipientsvalue,
+        subject: subjectvalue,
+        body: bodyvalue
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+      if(!result['error']) {
+        load_mailbox('sent');
+      }
+      else {
+        document.querySelector('#compose-view').firstChild.appendChild(p).innerHTML = `${result['error']}`;
+      }
+  });
+};
